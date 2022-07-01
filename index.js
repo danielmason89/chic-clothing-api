8e216aed55d0e2b2f5a405115a29b71f5400383e
Daniel Mason <danmasson0@gmail.com>
2022-07-01 02:11:19 -0400

    initial commit - create express app and initiate node.js mongoDB connection

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connection successful!!"))
+  .catch((error) => {
+    console.log("there has been a error", error);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend Application is running");
+});
